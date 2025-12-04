cc8b229fd0b17baebae96ffc21dc96e28fbeba34
Loueny <loueny.rossi@aluno.senai.br>
2025-12-04 09:41:56 -0300

    Autenticar login

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ if (!isset($_SESSION['usuario_id'])) {
     header ('Location: login.php');
     exit;
 }
+
+$usuario_id = $_SESSION['usuario_id'];
+$usuario_nome = $_SESSION['usuario_nome'];
+
 ?>
 
 <!DOCTYPE html>
@@ -17,5 +21,9 @@ if (!isset($_SESSION['usuario_id'])) {
 </head>
 <body>
     <h1>Sistema Financeiro</h1>
+
+    <div>
+        <p>Bem-vindo, <strong><?= $usuario_nome ?></strong></p>
+    </div>
 </body>
 </html>
