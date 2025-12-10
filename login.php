c4fb52fb7fd4bd4008854996e3d728db73a86712
Loueny <loueny.rossi@aluno.senai.br>
2025-12-10 16:48:17 -0300

    hora de ir embora

--- login.php
+++ login.php
@@ -17,7 +17,6 @@ if (isset($_SESSION['usuario_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Sistema Financeiro</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
